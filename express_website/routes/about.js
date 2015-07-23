cfbbcd67f9a2de41ab1e5e55ea79c5a67cda0ee1
Robert Vargas <vargasvisuals@gmail.com>
2015-07-23 15:46:16 -0400

    sync new computer

--- express_website/routes/about.js
+++ express_website/routes/about.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var express = require('express');
 var router = express.Router();
 
@@ -8,14 +7,3 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
-=======
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('about', { title: 'About' });
-});
-
-module.exports = router;
->>>>>>> 9bb5bdbfd886b8305babff7d9512d123a00b702c
